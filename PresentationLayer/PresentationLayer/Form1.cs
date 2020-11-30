7e1881e1c90058a05b6f5ba8c099acaa0dba3abd
DESKTOP-MKD2AKH\Nesshhaa <nenadjankovic617@gmail.com>
2020-11-30 18:05:50 +0100

    Kreiran prezentacijski sloj

--- PresentationLayer/PresentationLayer/Form1.cs
+++ PresentationLayer/PresentationLayer/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer;
+using DataAccessLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,40 @@ namespace PresentationLayer
 {
     public partial class Form1 : Form
     {
+        private StudentBusiness studentBusiness;
         public Form1()
         {
+            this.studentBusiness = new StudentBusiness();
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void RefreshData()
+        {
+            List<Student> students = this.studentBusiness.GetAllStudents();
+            listBox1.Items.Clear();
+            foreach(Student s in students)
+            {
+                listBox1.Items.Add(s.Name + "\t" + s.IndexNumber + "\t" + s.AverageMark);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student s = new Student();
+            s.Name = textBox1.Text;
+            s.IndexNumber = textBox2.Text;
+            s.AverageMark = Convert.ToDecimal(textBox3.Text);
+            this.studentBusiness.InsertStudent(s);
+            RefreshData();
         }
     }
 }
